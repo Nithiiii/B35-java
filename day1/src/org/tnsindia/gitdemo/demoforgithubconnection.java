6832271b7bfa398edbba9e85661d96cf22e305be
Nithe <Nithe@NITHEEN>
2022-11-17 16:47:21 +0530

    first commit

---
+++ day1/src/org/tnsindia/gitdemo/demoforgithubconnection.java
@@ -0,0 +1,10 @@
+package org.tnsindia.gitdemo;
+// this prgrm is on cnnvtng eclipse wid github
+public class demoforgithubconnection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to tns india foundation");
+	}
+
+}
