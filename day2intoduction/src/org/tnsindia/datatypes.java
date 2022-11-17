e70c75b26c66240acb508ecf10917d585477acfb
Nithe <Nithe@NITHEEN>
2022-11-17 19:53:32 +0530

    first commit

---
+++ day2intoduction/src/org/tnsindia/datatypes.java
@@ -0,0 +1,37 @@
+package org.tnsindia;
+
+import java.util.Scanner;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+
+		
+		
+		String s="nitheen";
+		System.out.println("string is " +s);
+		
+		int y=122;
+		System.out.println("integer is " +y);
+		
+		
+		float f=128.8f;
+		System.out.println("float is " +f);
+		
+	    char c='n';
+		System.out.println("integer is " +c);
+		
+		double z=122;
+		System.out.println("double is " +z);
+		
+		boolean var=true;
+		System.out.println("boolean is " +var);
+		
+		
+		
+	}
+
+	
+	
+
+}
